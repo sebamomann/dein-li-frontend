08e3abeb0b073de94300adf5b4ce70868d1d3d31
sebamomann <github@sebamomann.de>
2020-12-21 23:20:40 +0100

    overview (matcher for empty query parameter)

--- src/app/components/link/link.module.ts
+++ src/app/components/link/link.module.ts
@@ -1,13 +1,30 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LinkComponent} from './link.component';
-import {RouterModule, Routes} from '@angular/router';
+import {RouterModule, Routes, UrlSegment} from '@angular/router';
 import {MatDialogModule, MatIconModule, MatSnackBarModule} from '@angular/material';
 import {AddVersionDialogComponent} from '../../dialogs/add-version-dialog/add-version-dialog.component';
 import {AddVersionDialogModule} from '../../dialogs/add-version-dialog/add-version-dialog.module';
 
 const routes: Routes = [
-  {path: '', component: LinkComponent},
+  {
+    component: LinkComponent,
+    matcher: (url) => {
+      if (url.length === 1 && url[0].path.match(/^link\?l=/gm)) {
+        return {
+          consumed: url,
+          posParams: {
+            username: new UrlSegment(url[0].path.substr(1), {})
+          }
+        };
+      }
+      return null;
+    },
+  },
+  {
+    path: '',
+    loadChildren: './overview/overview.module#OverviewModule',
+  },
 ];
 
 @NgModule({
