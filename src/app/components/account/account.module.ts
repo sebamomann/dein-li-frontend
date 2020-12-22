c12dc06b6bca86841d876a6325fc6e33b3343084
sebamomann <github@sebamomann.de>
2020-12-22 20:27:09 +0100

    logout component

--- src/app/components/account/account.module.ts
+++ src/app/components/account/account.module.ts
@@ -1,14 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
+import {LogoutComponent} from './logout/logout.component';
 
 const routes: Routes = [
   {path: 'login', loadChildren: './login/login.module#LoginModule'},
+  {path: 'logout', component: LogoutComponent},
 ];
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LogoutComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
