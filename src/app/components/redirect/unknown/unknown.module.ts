75e163ee792ce332ad00d5c754e12172f1b62b42
sebamomann <github@sebamomann.de>
2020-12-22 20:17:45 +0100

    link unknown error message

--- src/app/components/redirect/unknown/unknown.module.ts
+++ src/app/components/redirect/unknown/unknown.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UnknownComponent } from './unknown.component';
+import {RouterModule, Routes} from '@angular/router';
+import {NotFoundComponent} from '../not-found/not-found.component';
+import {MatButtonModule, MatCardModule, MatIconModule} from '@angular/material';
 
-
+const routes: Routes = [
+  {path: '', component: UnknownComponent},
+];
 
 @NgModule({
   declarations: [UnknownComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
-export class UnknownModule { }
+export class UnknownModule {
+}
