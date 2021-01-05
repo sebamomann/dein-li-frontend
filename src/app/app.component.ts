5784481b2ab9189b320748cd7013f556aeab43ab
sebamomann <github@sebamomann.de>
2021-01-05 15:13:55 +0100

    new Tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import {Component} from '@angular/core';
 import {ToolbarService} from './services/toolbar.service';
+import {NavigationEnd, Router} from '@angular/router';
+import {filter} from 'rxjs/operators';
+
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -9,9 +13,16 @@ import {ToolbarService} from './services/toolbar.service';
 export class AppComponent {
   public title = 'Dein Link - In Kurzform';
 
-  constructor(private toolbarService: ToolbarService) {
+  constructor(private toolbarService: ToolbarService, router: Router) {
     this.toolbarService.title$.subscribe((sTitle) => {
       this.title = sTitle;
+    });
+
+    const navEndEvent$ = router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    );
+    navEndEvent$.subscribe((e: NavigationEnd) => {
+      gtag('config', 'MY_ID', {page_path: e.urlAfterRedirects});
     });
   }
 }
